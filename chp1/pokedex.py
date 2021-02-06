1417fe4f1e2487c5f91274a5b7175325c98a5ab8
Samuel Benidt <samuel.benidt@disney.com>
2021-02-06 17:02:45 -0500

    implement slicing for pokedex

--- chp1/pokedex.py
+++ chp1/pokedex.py
@@ -1,19 +1,43 @@
+"""Code to illustrate python data model by creating a pokedex class"""
+
 import collections
 
 # Create Pokemon Class
 Pokemon = collections.namedtuple(
     "Pokemon", ["name", "number", "type1", "type2", "height", "weight"])
 
+
 # Create Pokedex Class
 class Pokedex:
     def __init__(self):
         with open("pokemon_1st_gen.csv") as filen:
             pokemon_data = filen.read()
-            pokemon_data = pokemon_data.splitlines()
+            # skip header row
+            pokemon_data = pokemon_data.splitlines()[1:]
         self._pokemon = [Pokemon(*row.split(',')) for row in pokemon_data]
 
     def __len__(self):
         return len(self._pokemon)
 
     def __getitem__(self, position):
-        return self._pokemon[position]
+        # Support pokedex incides starting with 1 instead of 0
+        if isinstance(position, slice):
+            start = position.start - 1
+            stop = position.stop - 1
+            step = position.step
+            return self._pokemon[slice(start, stop, step)]
+        else:
+            return self._pokemon[position - 1]
+
+
+# Create a dex
+dex = Pokedex()
+
+# By implementing __len__ the pokedex has a length
+len(dex)
+
+# Look up the 121st pokemon
+dex[121]
+
+# slicing is supported
+dex[120:130]
